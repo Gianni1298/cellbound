f8c80793d68e2a10d2a73ec782f981054191c3c1
GianpiColonna <gcolonna@expediagroup.com>
2023-12-24 00:53:01 +0000

    Add setup file to allow the package to be installed using pip and to simplify code changes in the main package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cellbound',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT'
+    # other metadata like author, description, etc.
+)
